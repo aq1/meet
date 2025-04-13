73e542241a9a4e0ca0a6accf171d04e22ff344a4
aq1 <aq1@aq1.aq1>
2025-04-13 20:17:01 +0300

    x

---
+++ server/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from sio_app import sio_app
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.mount("/", sio_app)
