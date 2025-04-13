d1a79a4124d785e5cc0bcdf1d83f8b60eef0b989
aq1 <aq1@aq1.aq1>
2025-04-13 19:22:12 +0300

    x

--- update.sh
+++ update.sh
@@ -7,5 +7,5 @@ set +o allexport
 export RELEASE=$(git rev-parse --short=8 HEAD)
 
 docker build -t meet-server:${RELEASE} ./server
-cd client && bun run build && cd -
+cd client && bun install --frozen-lockfile && bun run build && cd -
 docker compose up -d
