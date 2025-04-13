4507fae95d080cfde143444bac7abf52d0682fea
aq1 <aq1@aq1.aq1>
2025-04-13 19:18:49 +0300

    x

--- update.sh
+++ update.sh
@@ -6,8 +6,6 @@ set +o allexport
 
 export RELEASE=$(git rev-parse --short=8 HEAD)
 
-git pull
-
 docker build -t meet-server:${RELEASE} ./server
 cd client && bun run build && cd -
 docker compose up -d
