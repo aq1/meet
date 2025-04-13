2ec88bf1d500b750991037578346a08ccca9dbdf
aq1 <aq1@aq1.aq1>
2025-04-13 19:18:33 +0300

    x

--- update.sh
+++ update.sh
@@ -8,6 +8,6 @@ export RELEASE=$(git rev-parse --short=8 HEAD)
 
 git pull
 
-docker build -t midi:${RELEASE} ./server
+docker build -t meet-server:${RELEASE} ./server
 cd client && bun run build && cd -
 docker compose up -d
