3b1c51edc5ff8c76d2ea5d54ef3264fd513dc79e
aq1 <no@email.com>
2026-06-13 14:56:52 +0300

    Login in room

--- src/routes/room/$roomId.tsx
+++ src/routes/room/$roomId.tsx
@@ -1,5 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
+import { LoginDialog } from "#/components/login-dialog/LoginDialog";
 import { VocalRoom } from "#/components/rooms/vocal/Room";
+import { useUser } from "#/lib/user-store";
 
 export const Route = createFileRoute("/room/$roomId")({
   component: RoomPage,
@@ -7,5 +10,16 @@ export const Route = createFileRoute("/room/$roomId")({
 
 function RoomPage() {
   const { roomId } = Route.useParams();
+  const username = useUser((state) => state.username);
+  const [showLoginDialog, setShowLoginDialog] = useState(true);
+
+  useEffect(() => {
+    setShowLoginDialog(!username);
+  }, [username]);
+
+  if (showLoginDialog) {
+    return <LoginDialog />;
+  }
+
   return <VocalRoom roomId={roomId} />;
 }
