988b2b1dfa02305f76679f3982f22865695a42b2
aq1 <no@email.com>
2026-06-27 16:24:15 +0300

    test

--- src/components/rooms/vocal/controls/StartAudioButton.tsx
+++ src/components/rooms/vocal/controls/StartAudioButton.tsx
@@ -1,19 +1,29 @@
-import { useRoomContext, useStartAudio } from "@livekit/components-react";
-import { Volume2Icon } from "lucide-react";
+import { useRoomContext, useStartAudio, useStartVideo } from "@livekit/components-react";
 import { Button } from "#/components/ui/button";
 
 export const StartAudioButton = () => {
   const room = useRoomContext();
-  const { mergedProps } = useStartAudio({ room, props: {} });
+  const { mergedProps: { onClick: OnStartAudio } } = useStartAudio({ room, props: {} });
+  const { mergedProps: { onClick: OnStartVideo } } = useStartVideo({ room, props: {} });
 
   return (
-    <Button
-      {...mergedProps}
-      variant="default"
-      size="icon-xl"
-      title="Start audio"
-    >
-      <Volume2Icon />
-    </Button>
+    <>
+      <Button
+        onClick={OnStartAudio}
+        variant="default"
+        size="icon-xl"
+        title="Start audio"
+      >
+        Audio
+      </Button>
+      <Button
+        onClick={OnStartVideo}
+        variant="default"
+        size="icon-xl"
+        title="Start audio"
+      >
+        Video
+      </Button>
+    </>
   );
 };
