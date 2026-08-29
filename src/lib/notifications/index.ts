486b7190dd869833a323fb170327f5e3aa666a28
aq1 <no@email.com>
2026-08-29 21:35:04 +0300

    tg fix

--- src/lib/notifications/index.ts
+++ src/lib/notifications/index.ts
@@ -3,16 +3,22 @@ import { env } from "#/env";
 type NotifyAdminsT = { text: string };
 
 export const notifyAdmins = async ({ text }: NotifyAdminsT) => {
-  env.TELEGRAM_ADMINS.forEach(async (chat_id) => {
+  env.TELEGRAM_ADMINS.forEach(async (chatId) => {
     const data = {
-      chat_id,
+      chat_id: chatId,
       text,
       disable_web_page_preview: true,
     };
 
     const res = await fetch(
       `https://api.telegram.org/bot${env.TELEGRAM_TOKEN}/sendMessage`,
-      { method: "POST", body: JSON.stringify(data) },
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      },
     );
 
     if (!res.ok) {
